6418f3aa5ac843ff60da1773dc736acf89de3445
marklewis27 <54063883+marklewis27@users.noreply.github.com>
2020-04-07 19:44:11 +0100

    Added in pymongo and connected to mongodb

--- env/app.py
+++ env/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from datetime import datetime
 from flask import render_template
+from flask_pymongo import PyMongo
+
 import re
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://me:Internal27@myfirstcluster-x6esz.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo= PyMongo(app)
 
 @app.route("/")
 def home():
