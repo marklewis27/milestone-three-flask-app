6684708d7a96df615a75c02f2a2014393d5080a9
marklewis27 <54063883+marklewis27@users.noreply.github.com>
2020-04-16 19:44:48 +0100

    update the vechile details is fully working now, the information is pre filled in and the options to edit are there and save the changes when the button at the bottom is clicked

--- env/app.py
+++ env/app.py
@@ -23,6 +23,7 @@ def home():
 @app.route("/new/")
 def new():
     return render_template("new.html",
+    gearboxes=mongo.db.gearboxes.find(),
     engines=mongo.db.engines.find(),
     fuels=mongo.db.fuels.find(),
     categories=mongo.db.categories.find())
@@ -37,12 +38,31 @@ def insert_car():
 def update(car_id):
     the_car = mongo.db.cars.find_one({"_id": ObjectId(car_id)})
     all_categories = mongo.db.categories.find()
+    all_gearboxes = mongo.db.gearboxes.find()
     all_engines = mongo.db.engines.find()
     all_fuels = mongo.db.fuels.find()
     return render_template("update.html",car=the_car,
+                            gearboxes=all_gearboxes,
                             engines=all_engines,
                             fuels=all_fuels,
                             categories=all_categories)
+
+@app.route('/changes/<car_id>', methods=["POST"])
+def changes(car_id):
+    cars = mongo.db.cars
+    cars.update( {'_id': ObjectId(car_id)},
+    {
+        'category_name':request.form.get('category_name'),
+        'car_make':request.form.get('car_make'),
+        'car_model':request.form.get('car_model'),
+        'fuel_type':request.form.get('fuel_type'),
+        'engine_size':request.form.get('engine_size'),
+        'gearbox_type':request.form.get('gearbox_type'),
+        'car_mileage':request.form.get('car_mileage'),
+        'car_colour':request.form.get('car_colour')
+    })
+    return redirect(url_for("home"))
+
 
 @app.route("/delete/")
 def delete():
