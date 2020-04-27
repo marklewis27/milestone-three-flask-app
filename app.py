c1cd3ad759d9752388711014ed38ff35bd3012eb
marklewis27 <54063883+marklewis27@users.noreply.github.com>
2020-04-27 16:17:11 +0100

    moved the information regarding the mongo db username and password in to the config vars in heroku

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import re
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://Mark:Markpassword@cluster0-x6esz.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo= PyMongo(app)
 
